80ac15a998a016f270609088cb5e767b1179b02b
Jyun-Neng <jyunnengji@gmail.com>
2017-09-28 15:11:33 +0800

    add new test file

--- ALU_tb.sv
+++ ALU_tb.sv
@@ -1,7 +1,26 @@
+/* 
+    * File Name : ALU_tb.sv
+    *
+    * Purpose : An ALU testbench. 
+    *
+    * Creation Date : 2017/09/27
+    *
+    * Last Modified : 2017/09/28
+    *
+    * Create By : Jyun-Neng Ji
+    *
+*/
+
 `timescale 1ns/10ps
 
 `include "ALU.sv"
 
+`define ADD 4'b0000
+`define SUB 4'b0001
+`define AND 4'b0010
+`define OR 4'b0011
+`define SLL 4'b0100
+`define ROTATE 4'b0101
 `define	DataSize	32
 `define	ALUopSize	4
 
@@ -29,6 +48,14 @@ initial begin
     #10 enable = 1'b1; src1 = 32'h5; src2 = 32'h2; OP = 3'b011;
     #10 enable = 1'b1; src1 = 32'h1; src2 = 32'h1; OP = 3'b100;
     #10 enable = 1'b1; src1 = 32'h1; src2 = 32'h1; OP = 3'b101;
+    #10 enable = 1'b1; src1 = 'd21; src2 = 'd46; OP = `ADD;
+    #10 enable = 1'b1; src1 = 'd648; src2 = 'd1035; OP = `SUB;
+    #10 enable = 1'b1; src1 = 'd1034; src2 = 'd3028; OP = `AND;
+    #10 enable = 1'b1; src1 = 'd110; src2 = 'd30548; OP = `OR;
+    #10 enable = 1'b1; src1 = 'd12356; src2 = 'd3; OP = `SLL;
+    #10 enable = 1'b1; src1 = 'd12345678; src2 = 'd4; OP = `ROTATE;
+    
+
 end
 
 	initial begin // Generate the waveform file
