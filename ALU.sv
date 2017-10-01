59c4a3d4b7f2fc925b04ffcc301d55267153e71f
Jyun-Neng <jyunnengji@gmail.com>
2017-10-01 12:45:27 +0800

    add SLT, SRL operation

--- ALU.sv
+++ ALU.sv
@@ -2,81 +2,63 @@
     * File Name : ALU.sv
     *
     * Purpose : Make an ALU component with two 32 bits data input ports (sr1, sr2),
-    *           one 4 bits control port (OP), one 1 bit enable port (enable),
+    *           one 4 bits control port (ALUType), one 1 bit reset port (rst),
     *           one 32 bits data output port (alu_result), and one 1 bit
-    *           overflow detection port (Overflow). And it can do 6 operations.
-    *           Addition, substraction, bitwise and, bitwise or, shift left,
-    *           and rotate.
+    *           Zero detect port (Zero), detect if src1 is equal to src2. And it can do 8 operations.
+    *           Addition, substraction, and, or, shift left, shift right, xor, and set less than.
     *
     * Creation Date : 2017/09/27
     *
-    * Last Modified : 2017/09/28
+    * Last Modified : 2017/10/01
     *
     * Create By : Jyun-Neng Ji
     *
 */
 
-`define ADD 4'b0000
-`define SUB 4'b0001
-`define AND 4'b0010
-`define OR 4'b0011
-`define SLL 4'b0100
-`define ROTATE 4'b0101
+`define ADD 'd0
+`define SUB 'd1
+`define SLL 'd2
+`define SLT 'd3
+`define XOR 'd4
+`define SRL 'd5
+`define OR  'd6
+`define AND 'd7
+`define NDEF 'd8
 
-module ALU(Overflow, alu_result, src1, src2, OP, enable);
+module ALU(Zero, alu_result, src1, src2, ALUType, rst);
     parameter DataSize = 32;
     parameter ALUopSize = 4;
        
     input [DataSize-1:0] src1, src2;
-    input [ALUopSize-1:0] OP;
-    input enable;
+    input [ALUopSize-1:0] ALUType;
+    input rst;
     output logic [DataSize-1:0] alu_result;
-    output logic Overflow;
+    output logic Zero;
     
-    logic [2*DataSize-1:0] temp;
-
-    assign temp = {src1, src1} >> (src2 % 32);
-
     always_comb begin
-        if (enable) begin
-            case (OP)
-                `ADD : begin  // ADD src1, src2
-                    alu_result = src1 + src2;
-                    if ((alu_result[31]==0 && src1[31]==1 && src2[31]==1)||(alu_result[31]==1 && src1[31]==0 && src2[31]==0))
-                        Overflow = 1;
-                    else Overflow = 0;
-                end
-                `SUB : begin  // SUB src1, src2
-                    alu_result = src1 - src2;
-                    if ((alu_result[31]==0 && src1[31]==1 && src2[31]==0)||(alu_result[31]==1 && src1[31]==0 && src2[31]==1))
-                        Overflow = 1;
-                    else Overflow = 0;
-                end
-                `AND : begin  // AND src1, src2
-                    alu_result = src1 & src2;
-                    Overflow = 0;
-                end
-                `OR : begin  // OR src1, src2
-                    alu_result = src1 | src2;
-                    Overflow = 0;
-                end
-                `SLL : begin  // SLL src1, src2
-                    alu_result = src1 << src2;
-                    Overflow = 0;
-                end
-                `ROTATE : begin  // Right rotate src1
-                    alu_result = temp[31:0];
-                    Overflow = 0;       
+        if (!rst) begin
+            case (ALUType)
+                `ADD : alu_result = src1 + src2;
+                `SUB : alu_result = src1 - src2;
+                `AND : alu_result = src1 & src2;
+                `OR : alu_result = src1 | src2;
+                `SLL : alu_result = src1 << (src2 % 32);
+                `SRL : alu_result = src1 >> (src2 % 32);
+                `SLT : alu_result = (src1 < src2) ? 1 : 0;
+                `XOR : begin
+                    alu_result <= src1 ^ src2;
+                    if (src1 == src2) Zero <= 1;
+                    else Zero <= 0;
                 end
                 default : begin
                     alu_result = 0;
-                    Overflow = 0;
+                    Zero = 0;
                 end
             endcase
         end
         else begin
             alu_result = 0;
-            Overflow = 0;
+            Zero = 0;
         end
     end
     
