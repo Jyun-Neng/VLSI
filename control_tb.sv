788a9691f75699ebab185ca34955202153d16713
Jyun-Neng <jyunnengji@gmail.com>
2017-10-02 10:18:15 +0800

    testbench

--- control_tb.sv
+++ control_tb.sv
@@ -16,18 +16,19 @@
 
 module control_tb;
     logic [6:0] OP;
-    logic DM_en, DM_write, RegWrite, branch, jump, ALUSrc;
+    logic rst, DM_en, DM_write, RegWrite, branch, jump, ALUSrc;
     logic [1:0] MemtoReg, ALUOp;
     logic [2:0] ExtenSel;
 
     control c0 (.OP(OP), .DM_en(DM_en), .DM_write(DM_write), .RegWrite(RegWrite), .branch(branch), .jump(jump), .ALUSrc(ALUSrc),
-                .MemtoReg(MemtoReg), .ALUOp(ALUOp), .ExtenSel(ExtenSel));
+                .MemtoReg(MemtoReg), .ALUOp(ALUOp), .ExtenSel(ExtenSel), .rst(rst));
 
-    initial $monitor($time, "OP=%b, DM_en=%b, DM_write=%b, branch=%b, jump=%b, ALUSrc=%b, MemtoReg=%b, RegWrite=%b, ALUOp=%d, ExtenSel=%d", 
-                    OP, DM_en, DM_write, branch, jump, ALUSrc, MemtoReg, RegWrite, ALUOp, ExtenSel);
+    initial $monitor($time, "rst=%b, OP=%b, DM_en=%b, DM_write=%b, branch=%b, jump=%b, ALUSrc=%b, MemtoReg=%b, RegWrite=%b, ALUOp=%d, ExtenSel=%d", 
+                   rst, OP, DM_en, DM_write, branch, jump, ALUSrc, MemtoReg, RegWrite, ALUOp, ExtenSel);
 
     initial begin
-        #10 OP = 'b0110011;
+        #0 rst = 0;
+        #10 rst = 1; OP = 'b0110011;
         #10 OP = 'b0010011;
         #10 OP = 'b0000011;
         #10 OP = 'b0100011;
